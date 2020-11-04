10820ecff37d204d8bfb65790f06f600d5d461ed
21shield <21netaly@gmail.com>
2020-11-04 17:54:27 -0500

    back server running

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require('express')
+// console.log(express)
+const app = express();
+// route handler
+app.get('/', (req, res) => {
+    // listen to a special http request like a "get", "post", "put", "delete", "patch"
+    // these are convention
+    // '/' route handler the ending / of local host ie: "/login", "/", "/signup"
+    res.send({hi: 'there'})
+    // the above  res.send sends back the info as json to the front
+
+});
+// run server for backend node >filename<
+// the port chosed to be the traffic input
+
+app.listen(5000)
+
