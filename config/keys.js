83f9eaa862629889603801ed340a2222c0ecc189
Netaly Ramirez <21netaly@gmail.com>
2020-11-05 17:56:51 -0500

    Delete keys.js

--- config/keys.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  googleClientID:
-    "529239086516-00i0m1ckkkkjsff6lrfpcfffdi9skrqe.apps.googleusercontent.com",
-  googleClientSecret: "pO236n4oLfyYyvygTWvGApgc",
-  mongoURI:
-    "mongodb+srv://nuts:RlThpacZB7YSBdsb@mern.lbdi5.mongodb.net/mernTest?retryWrites=true&w=majority",
-};
-
-// in the link given from mongo website replace password with given
